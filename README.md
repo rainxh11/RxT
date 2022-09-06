7a28ab64e3b0c63767c4ce682ce0b6e390a76ece
rainxh11 <rainxh11@gmail.com>
2022-09-06 14:02:51 +0100

    First beta version + Reactive<T> & Computed<T> working properly

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# RxT
+# Rx\<T>
+.NET Reactive Objects that provide similar functionalities to: (reactive, ref computed, watch) found in Vue.js
+
+*Documentations will be added soon...*
--- RxT/Reactive.cs
+++ RxT/Reactive.cs
@@ -1,92 +1,101 @@
 ﻿using System.ComponentModel;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
-using System.Runtime.CompilerServices;
-using RxT.Annotations;
 
-namespace RxT
+namespace RxT;
+
+/// <summary>
+///     Readonly Wrapper Reactive Object for Reactive<T>
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Computed<T> : IObservable<T>, INotifyPropertyChanged
 {
+    private readonly Func<IObservable<T>, IObservable<T>> _modifier;
+
+    private readonly Reactive<T> _source;
+
     /// <summary>
-    /// Readonly Wrapper Reactive Object for Reactive<T> 
+    ///     Create new Computed Reactive Object
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class Computed<T> : IObservable<T>, INotifyPropertyChanged
+    /// <param name="reactiveObject">Reactive Object by reference</param>
+    /// <param name="modifier">Modifier function to apply filters to the inner Observable withing 'reactiveObject' parameter</param>
+    public Computed(in Reactive<T> reactiveObject, Func<IObservable<T>, IObservable<T>> modifier)
     {
-        /// <summary>
-        /// Create new Computed Reactive Object
-        /// </summary>
-        /// <param name="reactiveObject">Reactive Object by reference</param>
-        /// <param name="modifier">Modifier function to apply filters to the inner Observable withing 'reactiveObject' parameter</param>
-        public Computed(in Reactive<T> reactiveObject, Func<IObservable<T>, IObservable<T>> modifier)
-        {
-            _source = reactiveObject;
-            _modifier = modifier;
-        }
-
-        private readonly Reactive<T> _source;
-
-        private readonly Func<IObservable<T>, IObservable<T>> _modifier;
-
-
-        public T Value => _source.Value;
-
-        public IDisposable Subscribe(IObserver<T> observer)
-        {
-            return _modifier(_source.InnerObservable)
-                .Do(_ => PropertyChanged?
-                    .Invoke(this,
-                        new PropertyChangedEventArgs(nameof(Value))))
-                .Subscribe(observer);
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
+        _source = reactiveObject;
+        _modifier = modifier;
     }
 
-    public class Reactive<T> : INotifyPropertyChanged, IObservable<T>
 
+    public T Value => _source.Value;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public IDisposable Subscribe(IObserver<T> observer)
     {
-        private T _value;
-        internal readonly IObservable<T> InnerObservable;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public static implicit operator T(Reactive<T> reactiveValue) => reactiveValue.Value;
-        public static explicit operator Reactive<T>(T obj) => new(obj);
-
-
-        public virtual IDisposable Subscribe(IObserver<T> observer) =>
-            InnerObservable
-                .Subscribe(observer);
-
-        /// <summary>
-        /// Trigger that 'Value' was changed and notify the Observable
-        /// </summary>
-        public virtual void TriggerChange() =>
-            PropertyChanged?
+        return _modifier(_source.InnerObservable)
+            .Do(_ => PropertyChanged?
                 .Invoke(this,
-                    new PropertyChangedEventArgs(nameof(Value)));
+                    new PropertyChangedEventArgs(nameof(Value))))
+            .Subscribe(observer);
+    }
+}
 
-        public virtual bool CanTriggerChange(T currentValue, T newValue) => true;
+public class Reactive<T> : INotifyPropertyChanged, IObservable<T>
 
-        public Reactive(T value)
+{
+    internal readonly IObservable<T> InnerObservable;
+    private T _value;
+
+    public Reactive(T value)
+    {
+        _value = value;
+        InnerObservable = Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                x => PropertyChanged += x,
+                x => PropertyChanged -= x)
+            .Select(_ => _value);
+    }
+
+    public T Value
+    {
+        get => _value;
+        set
         {
+            var canTriggerChange = CanTriggerChange(_value, value);
             _value = value;
-            InnerObservable = Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
-                    x => PropertyChanged += x,
-                    x => PropertyChanged -= x)
-                .Select(_ => _value);
+            if (canTriggerChange) TriggerChange();
         }
+    }
 
-        public T Value
-        {
-            get => _value;
-            set
-            {
-                var canTriggerChange = CanTriggerChange(_value, value);
-                _value = value;
-                if (canTriggerChange) TriggerChange();
-            }
-        }
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    public virtual IDisposable Subscribe(IObserver<T> observer)
+    {
+        return InnerObservable
+            .Subscribe(observer);
+    }
+
+    public static implicit operator T(Reactive<T> reactiveValue)
+    {
+        return reactiveValue.Value;
+    }
+
+    public static explicit operator Reactive<T>(T obj)
+    {
+        return new(obj);
+    }
+
+    /// <summary>
+    ///     Trigger that 'Value' was changed and notify the Observable
+    /// </summary>
+    public virtual void TriggerChange()
+    {
+        PropertyChanged?
+            .Invoke(this,
+                new PropertyChangedEventArgs(nameof(Value)));
+    }
+
+    public virtual bool CanTriggerChange(T currentValue, T newValue)
+    {
+        return true;
     }
 }
